89edfed25be1176dedacfb2107a1d64fa98eb7a5
Camila Ortolane <camilaortolane13@gmail.com>
2022-10-20 14:50:11 -0400

    complemento com a opc 1

--- WorkshopGit/Program.cs
+++ WorkshopGit/Program.cs
@@ -2,3 +2,22 @@
 
 Console.WriteLine("Olá, Mundo!");
 
+Console.WriteLine("---------------------------------");
+Console.WriteLine("[1] - Saudação.");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("escolha uma opção: ");
+int opc = int.Parse(Console.ReadLine());
+
+if(opc == 1)
+{
+    Console.WriteLine("Qual é seu nome? ");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine($"Olá {nome}!");
+}
+else
+{
+    Console.WriteLine("Tem nada aqui não");
+}
+
